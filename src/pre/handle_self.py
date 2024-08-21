078581bab9a3e29cbcf1241501e91855de4cbb69
eya46 <61458340+eya46@users.noreply.github.com>
2024-08-21 16:09:46 +0800

    fix handle_self.py

--- src/pre/handle_self.py
+++ src/pre/handle_self.py
@@ -6,10 +6,8 @@ from nonebot.adapters.onebot.v11 import Bot, Event, GroupMessageEvent, PrivateMe
 async def _(event: Event, bot: Bot):
     n = event.model_dump()
     if n.get("message_type") == "private":
-        n.post_type = "message"
-        n = PrivateMessageEvent.model_validate(n)
-        await bot.handle_event(n)
+        n["post_type"] = "message"
+        await bot.handle_event(PrivateMessageEvent.model_validate(n))
     elif n.get("message_type") == "group":
-        n.post_type = "message"
-        n = GroupMessageEvent.model_validate(n)
-        await bot.handle_event(n)
+        n["post_type"] = "message"
+        await bot.handle_event(GroupMessageEvent.model_validate(n))
