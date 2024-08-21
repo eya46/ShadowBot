d78a613da8fed5031a7fa8095790ffd672e96d64
eya46 <61458340+eya46@users.noreply.github.com>
2024-08-21 16:57:42 +0800

    提高handle_self优先级

--- src/pre/handle_self.py
+++ src/pre/handle_self.py
@@ -2,7 +2,7 @@ from nonebot import on
 from nonebot.adapters.onebot.v11 import Bot, Event, GroupMessageEvent, PrivateMessageEvent
 
 
-@on("message_sent", block=True).handle()
+@on("message_sent", block=True, priority=0).handle()
 async def _(event: Event, bot: Bot):
     n = event.model_dump()
     if n.get("message_type") == "private":
