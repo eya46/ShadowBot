b719ca54005eb95a69c1ed750d3b4ab2852e5c56
eya46 <61458340+eya46@users.noreply.github.com>
2024-08-21 11:29:32 +0800

    更换弃用的copy方法

--- src/pre/handle_self.py
+++ src/pre/handle_self.py
@@ -4,12 +4,12 @@ from nonebot.adapters.onebot.v11 import Bot, Event, GroupMessageEvent, PrivateMe
 
 @on("message_sent", block=True).handle()
 async def _(event: Event, bot: Bot):
-    n = event.copy()
-    if n.message_type == "private":
+    n = event.model_dump()
+    if n.get("message_type") == "private":
         n.post_type = "message"
-        n = PrivateMessageEvent.model_validate(n.model_dump())
+        n = PrivateMessageEvent.model_validate(n)
         await bot.handle_event(n)
-    elif n.message_type == "group":
+    elif n.get("message_type") == "group":
         n.post_type = "message"
-        n = GroupMessageEvent.model_validate(n.model_dump())
+        n = GroupMessageEvent.model_validate(n)
         await bot.handle_event(n)
